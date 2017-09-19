8e463817742fdd83ee7811245631cbcb303c0042
jurjenborkent <jurjenborkent@gmail.com>
2017-09-19 14:52:48 +0200

    Eerste commit

---
+++ src/pages/new-place/new-place.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PlacesService } from '../../services/places.service';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-new-place',
+  templateUrl: 'new-place.html',
+})
+export class NewPlacePage {
+
+  constructor(private placesService: PlacesService, private navCtrl: NavController) {}
+
+  onAddPlace(value: {title: string}) {
+  this.placesService.addPlace(value);
+  this.navCtrl.pop();
+  }
+
+}
