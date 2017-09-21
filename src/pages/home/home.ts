72e45abad9cce0b9bdca04d3f239628a0a01dc37
jurjenborkent <jurjenborkent@gmail.com>
2017-09-21 11:01:20 +0200

    menu die naar voren komt als je op een list item drukt

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NewPlacePage } from '../new-place/new-place';
 import { PlacesService } from '../../services/places.service';
+import { Platform, ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -14,11 +15,44 @@ export class HomePage {
     assignedTo: string
   }[] = [];
 
-
-
-  constructor(public navCtrl: NavController, public placesService: PlacesService) {
+  constructor(public navCtrl: NavController, public placesService: PlacesService, public platform: Platform,
+    public actionsheetCtrl: ActionSheetController) {
 
 }
+
+openMenu() {
+   let actionSheet = this.actionsheetCtrl.create({
+     title: 'Actie voor taak',
+     cssClass: 'action-sheets-basic-page',
+     buttons: [
+       {
+         text: 'Doing',
+         role: 'destructive',
+         icon: !this.platform.is('ios') ? 'trash' : null,
+         handler: () => {
+           console.log('Delete clicked');
+         }
+       },
+       {
+         text: 'Verwijder',
+         icon: !this.platform.is('ios') ? 'share' : null,
+         handler: () => {
+           console.log('Share clicked');
+         }
+       },
+       {
+         text: 'Annuleer',
+         role: 'cancel', // will always sort to be on the bottom
+         icon: !this.platform.is('ios') ? 'close' : null,
+         handler: () => {
+           console.log('Cancel clicked');
+         }
+       }
+     ]
+   });
+   actionSheet.present();
+ }
+
 
 ionViewWillEnter() {
   this.places = this.placesService.getPlaces();
